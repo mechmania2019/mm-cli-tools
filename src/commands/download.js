8685857e784fdeaa50d67b981ac18e3c732d1715
arshiam2 <arshiam2@illinois.edu>
2018-09-18 23:41:57 -0500

    downloading visualizer works

--- src/commands/download.js
+++ src/commands/download.js
@@ -2,9 +2,18 @@
 const path = require("path");
 const chalk = require("chalk");
 const execa = require("execa");
+const os = require('os');
+const fs = require('fs')
+const { promisify } = require('util')   
 
 const { getTeam } = require("../utils/auth");
 const handleErrors = require("../utils/handleErrors");
+
+const fileName = "visualizer.tar.gz"
+const visualizerPath = path.join(os.homedir(), '.mm/visualizer/')
+
+const stat = promisify(fs.stat)
+const mkdir = promisify(fs.mkdir)
 
 module.exports.command = "download";
 module.exports.describe =
@@ -21,16 +30,36 @@ module.exports.handler = handleErrors(async argv => {
     );
   }
   if (process.platform === "darwin") {
-    await execa('curl', ["-o", "mac.zip", 'mm-mac.now.sh']);
-    console.log("Downloading the visualizer for mac");
+    console.log("Starting download for the visualizer for mac");
+    try {
+      await stat(visualizerPath);
+    } catch(e) {
+      await mkdir(visualizerPath)
+    }
+    await execa('curl', ["-o", visualizerPath + "visualizer.tar.gz", "-ssL", 'mm-mac.now.sh']);
+    await execa('tar',['xvzf', visualizerPath + "visualizer.tar.gz","-C", visualizerPath]);
+    console.log("Successfully downloaded visualizer");
   } else if (process.platform === "win32" || process.platform === "win64") {
-    await execa('curl', ["-o", "mac.zip", 'mm-windows.now.sh']);
-    console.log("Downloading the visualizer for Windows 32");
+    console.log("Starting download for the visualizer for Windows");
+    try {
+      await stat(visualizerPath);
+    } catch(e) {
+      await mkdir(visualizerPath)
+    }
+    await execa('curl', ["-o", visualizerPath + "visualizer.tar.gz", "-ssL", 'mm-window.now.sh']);
+    await execa('tar',['xvzf', visualizerPath + "visualizer.tar.gz","-C", visualizerPath]);
+    console.log("Successfully downloaded visualizer");
   } else if (process.platform === "linux") {
-    await execa('curl', ["-o", "mac.zip", 'mm-linux.now.sh']);
-    console.log("Downloading the visualizer for Windows 64");
+    console.log("Starting download for the visualizer for Linux");
+    try {
+      await stat(visualizerPath);
+    } catch(e) {
+      await mkdir(visualizerPath)
+    }
+    await execa('curl', ["-o", visualizerPath + "visualizer.tar.gz", "-ssL", 'mm-linux.now.sh']);
+    await execa('tar',['xvzf', visualizerPath + "visualizer.tar.gz","-C", visualizerPath]);
+    console.log("Successfully downloaded visualizer");
   } else {
     console.log("Your operating system is not supported");
   }
-  console.log(`Team ${chalk.red(team.name)}`);
 });

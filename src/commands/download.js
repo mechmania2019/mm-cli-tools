f36549970b8ac98aa8b2bac6a4dcb096a22ea09a
arshiam2 <arshiam2@illinois.edu>
2018-09-19 00:15:42 -0500

    cleaned up donwload logic

--- src/commands/download.js
+++ src/commands/download.js
@@ -2,18 +2,21 @@
 const path = require("path");
 const chalk = require("chalk");
 const execa = require("execa");
-const os = require('os');
-const fs = require('fs')
-const { promisify } = require('util')   
+const fetch = require("node-fetch");
+const os = require("os");
+const fs = require("fs");
+const tar = require("tar");
+const { promisify } = require("util");
+const rimraf = promisify(require("rimraf"));
 
-const { getTeam } = require("../utils/auth");
 const handleErrors = require("../utils/handleErrors");
 
-const fileName = "visualizer.tar.gz"
-const visualizerPath = path.join(os.homedir(), '.mm/visualizer/')
+// const visualizerFName = "visualizer"
+const visualizerDir = path.join(os.homedir(), ".mm", "visualizer");
+// const visualizerPath = path.join(visualizerDir, "visualizer.tar.gz");
 
-const stat = promisify(fs.stat)
-const mkdir = promisify(fs.mkdir)
+const stat = promisify(fs.stat);
+const mkdir = promisify(fs.mkdir);
 
 module.exports.command = "download";
 module.exports.describe =
@@ -22,37 +25,50 @@ module.exports.describe =
 module.exports.builder = yargs => yargs;
 
 module.exports.handler = handleErrors(async argv => {
+  try {
+    await rimraf(visualizerDir);
+  } catch (e) {}
+  try {
+    await mkdir(visualizerDir);
+  } catch (e) {}
+
   if (process.platform === "darwin") {
-    console.log("Starting download for the visualizer for mac");
-    try {
-      await stat(visualizerPath);
-    } catch(e) {
-      await mkdir(visualizerPath)
-    }
-    await execa('curl', ["-o", visualizerPath + "visualizer.tar.gz", "-ssL", 'mm-mac.now.sh']);
-    await execa('tar',['xvzf', visualizerPath + "visualizer.tar.gz","-C", visualizerPath]);
-    console.log("Successfully downloaded visualizer");
-  } else if (process.platform === "win32" || process.platform === "win64") {
-    console.log("Starting download for the visualizer for Windows");
-    try {
-      await stat(visualizerPath);
-    } catch(e) {
-      await mkdir(visualizerPath)
-    }
-    await execa('curl', ["-o", visualizerPath + "visualizer.tar.gz", "-ssL", 'mm-window.now.sh']);
-    await execa('tar',['xvzf', visualizerPath + "visualizer.tar.gz","-C", visualizerPath]);
-    console.log("Successfully downloaded visualizer");
-  } else if (process.platform === "linux") {
-    console.log("Starting download for the visualizer for Linux");
-    try {
-      await stat(visualizerPath);
-    } catch(e) {
-      await mkdir(visualizerPath)
-    }
-    await execa('curl', ["-o", visualizerPath + "visualizer.tar.gz", "-ssL", 'mm-linux.now.sh']);
-    await execa('tar',['xvzf', visualizerPath + "visualizer.tar.gz","-C", visualizerPath]);
-    console.log("Successfully downloaded visualizer");
-  } else {
-    console.log("Your operating system is not supported");
+    console.log("Downloading the game");
+    fetch("https://mm-mac.now.sh").then(res =>
+      res.body.pipe(
+        tar.x({
+          strip: 1,
+          C: visualizerDir
+        })
+      )
+    );
   }
+
+  if (process.platform === "win32" || process.platform === "win64" ) {
+    console.log("Downloading the game");
+    fetch("https://mm-window.now.sh").then(res =>
+      res.body.pipe(
+        tar.x({
+          strip: 1,
+          C: visualizerDir
+        })
+      )
+    );
+  }
+
+
+  if (process.platform === "linux") {
+    console.log("Downloading the game");
+    fetch("https://mm-linux.now.sh").then(res =>
+      res.body.pipe(
+        tar.x({
+          strip: 1,
+          C: visualizerDir
+        })
+      )
+    );
+  }
+
+
+ 
 });

7a5bb8187166ed723743d2127beeaa6c6b82736d
Pranay Prakash <pranay.gp@gmail.com>
2018-09-19 15:09:33 -0500

    change registration log message

--- src/commands/register.js
+++ src/commands/register.js
@@ -1,32 +1,38 @@
 // @flow
-const path = require('path')
-const { promisify } = require('util')
+const path = require("path");
+const { promisify } = require("util");
 
-const chalk = require('chalk');
+const chalk = require("chalk");
 
-const { register } = require('../utils/auth')
-const { createRL, cmdQuestion } = require('../utils/prompt')
-const handleErrors = require('../utils/handleErrors')
+const { register } = require("../utils/auth");
+const { createRL, cmdQuestion } = require("../utils/prompt");
+const handleErrors = require("../utils/handleErrors");
 
-module.exports.command = 'register'
-module.exports.describe = 'Create a new team'
+module.exports.command = "register";
+module.exports.describe = "Create a new team";
 
-module.exports.builder = (yargs: any) => yargs
+module.exports.builder = (yargs: any) => yargs;
 
 module.exports.handler = handleErrors(async (argv: {}) => {
   const close = createRL();
   // prompt Team name and Email
-  const name = await cmdQuestion('Team Name: ')
-  const email = await cmdQuestion('Email (used for communication during/after the tournament): ')
+  const name = await cmdQuestion("Team Name: ");
+  const email = await cmdQuestion(
+    "Email (used for communication during/after the tournament): "
+  );
 
   const team = await register(name, email);
 
-  if(!team) {
-    console.log('Failed to register - Team may already exist')
+  if (!team) {
+    console.log(
+      "Failed to register - Team name may already exist or registration is closed"
+    );
   } else {
-    console.log('Registered your team as %s', team.name)
-    console.log(`Ask your friends to run \`mm login ${chalk.blue(team.token)}\` to join`)
+    console.log("Registered your team as %s", team.name);
+    console.log(
+      `Ask your friends to run \`mm login ${chalk.blue(team.token)}\` to join`
+    );
   }
 
-  close()
+  close();
 });

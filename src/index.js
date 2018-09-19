37780d07071515ef676f8c533cb26f5538d0ce62
Pranay Prakash <pranay.gp@gmail.com>
2018-09-19 03:15:29 -0500

    add version checks

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
 #!/usr/bin/env node
 //@flow
 const yargs = require('yargs');
+const pkg = require('../package');
+const checkForUpdate = require('update-check');
 
 const config = require('./utils/config');
 const { getLocalVersion } = require('./utils/version');
 
 (async () => {
+  let update = null;
+
+  try {
+    update = await checkForUpdate(pkg);
+  } catch (err) {
+    console.error(`Failed to check for updates: ${err}`);
+  }
+
+  if (update) {
+    console.log(`The latest version is ${update.latest}. Please update!`);
+  }
   await config.setup()
   const argv = yargs
     .commandDir('./commands')
     .alias('h', 'help')
     .alias('v', 'version')
-    .version(await getLocalVersion())
+    .version(getLocalVersion())
     .argv;
 })()
--- src/utils/version.js
+++ src/utils/version.js
@@ -1,18 +1,9 @@
 // @flow
 
-const config = require('./config')
-const fs = require('fs')
-const { promisify } = require('util')
+const pkg = require('../../package');
 
-const readFile = promisify(fs.readFile)
-const writeFile = promisify(fs.writeFile)
-
-const versionFile = config('VERSION')
-
-const getLocalVersion = async () => await readFile(versionFile, 'utf8')
-const setLocalVersion = async (version: string) => await writeFile(versionFile, version)
+const getLocalVersion = () => pkg.version
 
 module.exports = {
-  getLocalVersion,
-  setLocalVersion
+  getLocalVersion
 }

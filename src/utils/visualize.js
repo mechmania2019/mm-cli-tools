ec73be8fd3b1e720526834006e8eaef45f32066c
Pranay Prakash <pranay.gp@gmail.com>
2018-09-20 17:46:26 -0500

    0.0.13

--- src/utils/visualize.js
+++ src/utils/visualize.js
@@ -4,24 +4,24 @@ const run = require("./run");
 const appRoot = require("app-root-path");
 
 const VISUALIZER_DIR = path.join(os.homedir(), ".mm", "visualizer");
-const GAME_NAME = os => (os === "Windows" && `MM2018`) || `MM_${os}_After5`;
+const GAME_NAME = `MM2018`;
 
 const getVisualizer = () => {
   switch (process.platform) {
     case "darwin":
-      return path.join(VISUALIZER_DIR, "Contents", "MacOS", GAME_NAME("MacOS"));
+      return path.join(VISUALIZER_DIR, "Contents", "MacOS", GAME_NAME);
       break;
     case "win32":
     case "win64":
-      return path.join(VISUALIZER_DIR, `${GAME_NAME("Windows")}.exe`);
+      return path.join(VISUALIZER_DIR, `${GAME_NAME}.exe`);
       break;
     case "linux":
       if (process.arch === "x64")
         return path.join(
           VISUALIZER_DIR,
-          `${GAME_NAME("LinuxCombined")}.x86_64`
+          `${GAME_NAME}.x86_64`
         );
-      return path.join(VISUALIZER_DIR, `${GAME_NAME("LinuxCombined")}.x86_64`);
+      return path.join(VISUALIZER_DIR, `${GAME_NAME}.x86`);
       break;
   }
 };

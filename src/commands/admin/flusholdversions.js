37eaea08da7684f4ca1df3f4e09d4382f3befa1c
njc1583 <njc1583@gmail.com>
2019-09-19 15:14:46 -0500

    Refractor flusholdversions.js

--- src/commands/admin/flusholdversions.js
+++ src/commands/admin/flusholdversions.js
@@ -6,9 +6,9 @@ const inquirer = require("inquirer");
 
 const { getTeam } = require("../../utils/auth");
 const handleErrors = require("../../utils/handleErrors");
-const { queueall } = require("../../api");
+const { flusholdversions } = require("../../api");
 
-module.exports.command = "queue-all";
+module.exports.command = "flush-old-versions";
 module.exports.describe = false;
 
 module.exports.builder = yargs => yargs;
@@ -26,7 +26,7 @@ module.exports.handler = handleErrors(async () => {
     console.log("Flushing old scripts...");
 
     try {
-        console.log(await queueall(team));
+        console.log(await flusholdversions(team));
         console.log(
             chalk.blue(
                 "Old versions are flushed!"
